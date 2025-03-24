c173ef1c101345841c1490f574d4d23d5728bce2
trex47 <somsamaidoodexlaos@gmail.com>
2025-03-24 09:36:23 +0700

    a
    1

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Css/App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <p>hello words</p>
+      <button className="btn btn-primary">Click Me</button>
+    </div>
+  );
+}
+
+export default App;
